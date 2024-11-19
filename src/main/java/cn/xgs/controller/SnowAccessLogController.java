3fb7a94a51aa48ba34dac186e175f6bddbfd40ea
XiaoHu <1583139786@qq.com>
2024-11-19 19:38:09 +0800

    feat():新增日志控制层

--- src/main/java/cn/xgs/controller/SnowAccessLogController.java
+++ src/main/java/cn/xgs/controller/SnowAccessLogController.java
@@ -1,21 +1,33 @@
 package cn.xgs.controller;
 
-import cn.xgs.dao.SnowAccessLog;
-import cn.xgs.service.SnowAccessService;
+import cn.xgs.domain.dto.SnowAccessLog;
+import cn.xgs.service.ISnowAccessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class SnowAccessLogController {
 
     @Autowired
-    private SnowAccessService snowAccessService;
+    private ISnowAccessService snowAccessService;
 
-    @GetMapping("/openapi/snow/accesslog")
-    public List<SnowAccessLog> accesslog() {
-        return snowAccessService.getSnowAccessLogs();
+    @GetMapping("/log/list")
+    public List<SnowAccessLog> list() {
+        return snowAccessService.list();
+    }
+    @GetMapping("/log/add")
+    public Integer add() {
+        SnowAccessLog snowAccessLog = new SnowAccessLog();
+        snowAccessLog.setData("G42 K-97898988发起救援服务！！！");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        snowAccessLog.setTime(simpleDateFormat.format(new Date()));
+        snowAccessLog.setTag("测试");
+        return snowAccessService.insert(snowAccessLog);
     }
 }
---
+++ src/main/java/cn/xgs/domain/vo/LogVo.java
@@ -0,0 +1,12 @@
+package cn.xgs.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogVo {
+    private String data;
+}
