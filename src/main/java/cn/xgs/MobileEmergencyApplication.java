7954034e5cd894417163f1915f7adf4e57026d07
XiaoHu <1583139786@qq.com>
2024-11-19 11:48:13 +0800

    feat():新增项目启动后端入口

---
+++ src/main/java/cn/xgs/MobileEmergencyApplication.java
@@ -0,0 +1,27 @@
+package cn.xgs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@Slf4j
+public class MobileEmergencyApplication {
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext run = SpringApplication.run(MobileEmergencyApplication.class, args);
+        Environment env = run.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "MobileEmergencyApplication is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t" +
+                "Swagger3文档: \thttp://" + ip + ":" + port + "/swagger-ui/index.html\n" +
+                "----------------------------------------------------------");
+    }
+}
