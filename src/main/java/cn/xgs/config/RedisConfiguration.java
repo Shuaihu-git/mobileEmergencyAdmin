9fe16cf419e587448d6c5e77de124701ab0c77da
XiaoHu <1583139786@qq.com>
2024-11-19 14:27:55 +0800

    feat():新增Redis配置

---
+++ src/main/java/cn/xgs/config/RedisConfiguration.java
@@ -0,0 +1,92 @@
+package cn.xgs.config;
+
+import cn.hutool.core.util.ReflectUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.NettyCustomizer;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 配置类
+ * @author XiaoHu
+ */
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(name="lettuce.check-enabled",havingValue = "true")
+    public ClientResources clientResources(){
+
+        NettyCustomizer nettyCustomizer = new NettyCustomizer() {
+
+            @Override
+            public void afterChannelInitialized(Channel channel) {
+                channel.pipeline().addLast(
+                        new IdleStateHandler(30, 30, 30));
+                channel.pipeline().addLast(new ChannelDuplexHandler() {
+                    @Override
+                    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
+                        if (evt instanceof IdleStateEvent) {
+                            ctx.disconnect();
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void afterBootstrapInitialized(Bootstrap bootstrap) {
+            }
+
+        };
+
+        return ClientResources.builder().nettyCustomizer(nettyCustomizer ).build();
+    }
+
+
+
+    /**
+     * 创建 RedisTemplate Bean，使用 JSON 序列化方式
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        // 创建 RedisTemplate 对象
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 设置 RedisConnection 工厂
+        template.setConnectionFactory(factory);
+
+        FastJson2JsonRedisSerializer serializer = new FastJson2JsonRedisSerializer(Object.class);
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    public static RedisSerializer<?> buildRedisSerializer() {
+        RedisSerializer<Object> json = RedisSerializer.json();
+        // 解决 LocalDateTime 的序列化
+        ObjectMapper objectMapper = (ObjectMapper) ReflectUtil.getFieldValue(json, "mapper");
+        objectMapper.registerModules(new JavaTimeModule());
+        return json;
+    }
+
+}
